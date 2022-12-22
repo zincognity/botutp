bae6624bf8ed3ea37187d9d01d3fbb403b623108
jesusreluz <jesusmanuelreluz@hotmail.com>
2022-12-22 15:34:04 -0500

    Integracion del comando deny, prueba 1

---
+++ Commands/Registro/deny.js
@@ -0,0 +1,50 @@
+const { ChatInputCommandInteraction, SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits, RoleSelectMenuInteraction, ModalBuilder, ActionRowBuilder, TextInputComponent, TextInputBuilder, TextInputStyle, ModalSubmitFields, ModalSubmitInteraction } = require('discord.js');
+const registerSchema = require(`${process.cwd()}/DataBase/registerSchema`);
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('deny')
+    .setDescription('Denegarás los datos de un usuario.')
+    .addUserOption((option) => option.setName('target').setDescription('Usuario a denegar').setRequired(true))
+    .addStringOption((option) => option.setName('razon').setDescription('Razon de la negación'))
+    .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
+        /**
+     * 
+     * @param {ChatInputCommandInteraction} interaction 
+     */
+
+    async execute(interaction, client) {
+
+        const user = interaction.options.getUser('target');
+        const { guild } = interaction;
+
+        let razon = interaction.options.getString('razon');
+        const member = await interaction.guild.members.fetch(user.id).catch(console.error);
+
+        if (!razon) razon = 'Sin razón'
+        if (user.id === interaction.user.id) return interaction.reply({content: 'No puedes denegarte a ti mismo.', ephemeral: true});
+        if (user.id === client.user.id) return interaction.reply({content: 'No puedes denegarme a mi.', ephemeral: true});
+
+        const canal = interaction.guild.channels.cache.get('1054294724741189642');
+
+        try{
+            let iddc = registerSchema.findOne({
+                _id: user.id
+            });
+
+            if(!iddc){
+                return interaction.reply({content: 'No puedes denegar a un usuario que aún no se ha registrado!'});
+            } else{
+                iddc.deleteOne({
+                    _id: user.id
+                });
+
+                return interaction.reply({content: 'Los datos han sido eliminados correctamente.'});
+            }
+        } catch (err){
+            console.log(err);
+        }
+
+
+    }
+};
