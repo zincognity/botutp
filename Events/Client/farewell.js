b62012d97723db952440d3c606f5e98ae0c2b3b3
jesusreluz <jesusmanuelreluz@hotmail.com>
2022-12-23 00:07:10 -0500

    Evento de despedidas

--- Events/Client/farewell.js
+++ Events/Client/farewell.js
@@ -1,29 +1,28 @@
 const { ChatInputCommandInteraction, ActionRow, ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, WelcomeScreen, AttachmentBuilder, EmbedBuilder } = require('discord.js');
 const { Welcome } = require('niby-welcomes');
 module.exports = {
-    name: 'guildMemberAdd',
+    name: 'guildMemberRemove',
     once: true,
     async execute(member){
         try{
             const { guild } = member;
 
-            let canalBienvenida = guild.channels.cache.get('1033216478209646633');
+            let canalDespedidas = guild.channels.cache.get('1033216515492823080');
             let welcomeImage = await new Welcome()
-                .setWelcomeMessage('BIENVENID@')
+                .setWelcomeMessage('ADIÓS')
                 .setUsername(member.user.tag, {color: '#D3052D'})
-                // .setMemberCount(`Eres el número #${member.guild.memberCount}`, {color: '#ffffff'})
                 .setAvatar(member.user.displayAvatarURL({size: 512, extension: 'png'}))
-                .setBackgroundUrl('https://i.imgur.com/uTitBxz.jpg', {opacity: 0})
+                .setBackgroundUrl('https://i.imgur.com/uTitBxz.jpg', {opacity: 0.2})
                 .setBorder(true, {color: '#D3052D', size: 5})
                 .setStyle('koya')
                 .build();
 
             let attachment = new AttachmentBuilder(welcomeImage, {name: `bienvenida-${member.user.tag}.png`});
 
-            canalBienvenida.send({content: `${member}, esperamos que te agrade la estadía en el servidor **${guild.name}. ;)**`,
+            canalDespedidas.send({content: `${member} ha dejado el servidor.`,
                 files: [attachment]
             }).catch((err) => {console.log(err)})
-            console.log(`guildMemberAdd: ${member}`);
+            console.log(`guildMemberRemove: ${member}`);
         } catch (err) {
             return console.log(err);
         }
