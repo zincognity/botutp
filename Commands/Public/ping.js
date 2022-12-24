38cfda15030f4efd79a59c2280e1243b0c4f940a
jesusreluz <jesusmanuelreluz@hotmail.com>
2022-12-23 22:56:16 -0500

    Ping update

--- Commands/Public/ping.js
+++ Commands/Public/ping.js
@@ -1,8 +1,4 @@
 const { ChatInputCommandInteraction, SlashCommandBuilder, GatewayIntentBits } = require('discord.js');
-const discord = require('discord.js');
-const client = new discord.Client({
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]
-});
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -14,7 +10,7 @@ module.exports = {
      */
     execute(interaction){
         interaction.reply({ content: `Pong!`, ephemeral: true}).then(m => {
-            m.interaction.editReply(`:incoming_envelope: Mi ping es: ${client.ws.ping} ms\`\ `);
+            m.interaction.editReply(`:incoming_envelope: Mi ping es: **${Date.now() - m.createdTimestamp} ms**. `);
         });
     },
 };
