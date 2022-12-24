e55abaeb0da39f216970db8a4216f80c1330d0aa
jesusreluz <jesusmanuelreluz@hotmail.com>
2022-12-23 19:46:27 -0500

    Integracion de application

---
+++ Events/Interactions/interactionCreate.js
@@ -0,0 +1,42 @@
+const { InteractionType } = require('discord.js');
+
+module.exports = {
+    name: 'interactionCreate',
+    async execute(interaction, client){
+        if (interaction.isChatInputCommand()){
+            const { commands } = client;
+            const { commandName } = interaction;
+            const command = commands.get(commandName);
+            if(!command)
+                return interaction.reply({
+                    content: 'This command is outdated',
+                    ephemeral: true,
+                });
+
+            if (command.developer && interaction.user.id !== "245702253971898379")
+                return interaction.reply({
+                    content: 'Este comando es solo para el developer.',
+                    ephemeral: true,
+                });
+
+            try{
+                await command.execute(interaction, client);
+            } catch(err){
+                console.error(err);
+                await interaction.reply({ content: 'Hubo un fallo al ejecutar este comando!', ephemeral: true });
+            }
+        } else if(interaction.isContextMenuCommand()){
+            const { commands } = client;
+            const { commandName } = interaction;
+            const contextCommand = commands.get(commandName);
+
+            if(!contextCommand) return;
+
+            try{
+                await contextCommand.execute(interaction, client);
+            }catch(err){
+                console.error(err);
+            }
+        }
+    },
+};
