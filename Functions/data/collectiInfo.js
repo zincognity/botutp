2945922e76cee54f01c9cc18f2884d6a52cb664d
jesusreluz <jesusmanuelreluz@hotmail.com>
2022-12-24 17:17:53 -0500

    Optimiced code Test 2

--- Functions/data/collectiInfo.js
+++ Functions/data/collectiInfo.js
@@ -2,7 +2,7 @@ const registerSchema = require(`${process.cwd()}/DataBase/registerSchema`);
 
 async function collectInfo(id, dato){
     let info = await registerSchema.findOne({ _id: id });
-    info[dato];
+    return info[`${dato}`];
 }
 
 module.exports = { collectInfo };
